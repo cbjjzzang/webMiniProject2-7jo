47b6dc2617f2d8eca258a7096e204d741551ee8c
Bongjin Choi <cbjjzzang@naver.com>
2022-04-08 17:04:39 +0900

    Initial commit

---
+++ src/main/java/com/sparta/webminiproject27jo/WebMiniProject27joApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.webminiproject27jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMiniProject27joApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMiniProject27joApplication.class, args);
+    }
+
+}
